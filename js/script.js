22dfc8318fcbfa1ad0f845fcd23757dd4d774250
Dovber <Dovberchik22@gmail.com>
2021-04-02 14:08:14 +0300

    frst commit

---
+++ js/script.js
@@ -0,0 +1,8 @@
+// const result = confirm('Are you here');
+// console.log(result);
+
+const category = 'toys';
+console.log(`https://someurl.com/${category}/5` );
+
+const user = 'Ivan';
+alert(`привет , ${user}`);
